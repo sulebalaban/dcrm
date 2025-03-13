053eb11af5fa568ca4ae7fad361ee17327f5ccf1
SULE BALABAN <sule.balaban27@gmail.com>
2025-03-13 13:25:39 +0100

    feat: Add login function and alert message

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 
 def home(request):
-    return render (request,'home.html',{})
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login (request,user)
+            messages.success(request,"You Have Been Logged In! ")
+            return redirect('home')
+        else:
+            messages.success(request,"Please Try Again")
+            return redirect('home')
+    else:    
+        return render (request,'home.html',{})
 
 def login_user(request):
     pass
