beaa2473362d3786ccef9af9b703c15774aacdd2
SULE BALABAN <sule.balaban27@gmail.com>
2025-03-16 16:53:58 +0100

    feat:Add register functions

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from.forms import SignUpForm
 
 def home(request):
     if request.method=='POST':
@@ -24,4 +25,16 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render (request,'register.html',{})
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request="You Have Succesfully Registered")
+            return redirect('home')
+    else:
+        form=SignUpForm    
+    return render (request,'register.html',{'form':form})
