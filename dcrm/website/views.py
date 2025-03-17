a7134f0e9abfa454ef0fdcc3716090f282ff8660
SULE BALABAN <sule.balaban27@gmail.com>
2025-03-17 11:51:14 +0100

    feat:Added record ıd link

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from.forms import SignUpForm
 from.models import Record
-from.models 
+
 
 
 def home(request):
